9f2ae7bc099717e17cc290ca88cf3f246e7a3d8b
Snehal Mane <snehal1999mane@gmail.com>
2023-12-02 15:23:55 +0530

    User add delete and view details functionality added

---
+++ src/users/AddUser.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function AddUser() {
+
+    let navigate=useNavigate();
+
+    const[user, setUser] = useState({
+        name:"",
+        username:"",
+        email:""
+    })
+
+    const{name,username,email}=user
+
+    const onInputChange=(e)=>{
+        setUser({...user,[e.target.name]:e.target.value})
+    };
+
+    const onSubmit=async(e)=>{
+        e.preventDefault();
+        await axios.post("http://localhost:8080/addUser",user)
+        navigate("/")
+    }
+
+  return (
+    <div className='container my-5'>
+        <div className='row'>
+            <div className='col-md-6 offset-md-3 border rounded p-2 mt-2 shadow'>
+                <h5 className='text-center m-4'>Register Users</h5>
+                <form onSubmit={(e)=>onSubmit(e)}>
+                <div className='mb-3'>
+                    <label htmlFor='Name' className='form-label'>
+                        Name
+                    </label>
+                   <input
+                   type={'text'}
+                   className='form-control'
+                   placeholder='Enter Name'
+                   name='name'
+                   value={name}
+                   onChange={(e)=>onInputChange(e)}/> 
+                   <label htmlFor='UserName' className='form-label'>
+                        Username
+                    </label>
+                   <input
+                   type={'text'}
+                   className='form-control'
+                   placeholder='Enter username'
+                   name='username'
+                   value={username}
+                   onChange={(e)=>onInputChange(e)}/> 
+                   <label htmlFor='Email' className='form-label'>
+                        Email
+                    </label>
+                   <input
+                   type={'text'}
+                   className='form-control'
+                   placeholder='Enter email'
+                   name='email'
+                   value={email}
+                   onChange={(e)=>onInputChange(e)}/> 
+                </div>
+                <button type='submit' className='btn btn-outline-primary'>Submit</button>
+                <Link className='btn btn-outline-danger mx-2' to='/'>Cancel</Link>
+                </form>
+            </div>
+        
+        </div> 
+    </div>
+  )
+}
